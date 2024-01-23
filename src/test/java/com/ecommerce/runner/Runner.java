1fa432af5041057e615046b7ea79c216469cb77e
Luis Ruiz <lruizajax@gmail.com>
2024-01-23 14:09:21 -0500

    Update Runner.java

--- src/test/java/com/ecommerce/runner/Runner.java
+++ src/test/java/com/ecommerce/runner/Runner.java
@@ -9,10 +9,11 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", 
 		 glue = "com.ecommerce.steps", 
-		 plugin = {"pretty", 
-			   "json:target/cucumber.json",
-			   "junit:target/cucumber.xml",
-			   "html:target/cucumber.html"
+		 plugin = {"pretty",
+			   "summary",
+			   "html:target/cucumber-reports/html-report.html",
+			   "json:target/cucumber-reports/cucumber.json",
+			   "junit:target/cucumber-reports/cucumber.xml",
 			  }, 
 		 dryRun = false, 
 		 monochrome = true)
